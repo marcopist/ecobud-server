fedf1710372675a44c7802983001ce7acd633d57
Marco Pistilli <pistilli.mrc@gmail.com>
2023-10-29 07:53:23 +0100

    Register webhook

---
+++ scripts/register_webhook.py
@@ -0,0 +1,8 @@
+from ecobud.connections.tink import register_transaction_webhook
+from ecobud.config import SELF_BASE_URL
+
+if __name__ == "__main__":
+    webhook_url = SELF_BASE_URL + "/tink/webhook"
+    print("Webhook URL:", webhook_url)
+    resp = register_transaction_webhook(webhook_url=webhook_url)
+    print("Response:", resp)
